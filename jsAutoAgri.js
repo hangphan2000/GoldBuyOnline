396d4fe372e8d842f98edca3d694e19483e30142
TrinhQuanVN <99104975+TrinhQuanVN@users.noreply.github.com>
2024-07-17 22:01:36 +0700

    Update jsAutoAgri.js

--- jsAutoAgri.js
+++ jsAutoAgri.js
@@ -1,14 +1,14 @@
-nhớ thêm dòng email vu.duyhai63@proton.me
----------------------------phan 1
+//nhớ thêm dòng email vu.duyhai63@proton.me
+//---------------------------phan 1
 // sửa thông tin tại đây
-const name = 'Vũ Duy Hải'; const t1= 500;
-const birthday = '16/03/1963'; const t2= 1000;
-const adress = 'Tổ 8 Minh Khai, Phủ Lý, hà nam'; const t3= 1500;
-const phone = '0972996973'; const t4= 200;
-const cmnd= '035063010610'; const t5= 2500;
-const noicap= 'cục cảnh sát qlhc về ttxh'; const t6= 3000;
-const ngaycap= '19/11/2021'; const t7= 3500;
-const tcheckbox = 4000;
+const name = 'Vũ Duy Hải'; const t1= 100;
+const birthday = '16/03/1963'; const t2= 100;
+const adress = 'Tổ 8 Minh Khai, Phủ Lý, hà nam'; const t3= 100;
+const phone = '0972996973'; const t4= 100;
+const cmnd= '035063010610'; const t5= 100;
+const noicap= 'cục cảnh sát qlhc về ttxh'; const t6= 100;
+const ngaycap= '19/11/2021'; const t7= 100;
+const tcheckbox = 100;
 //---------------------------------------
 function triggerEvent(el, type) {
   const event = new Event(type, { bubbles: true });
@@ -41,7 +41,6 @@ var timer = setInterval(function() {
           triggerEvent(input105, 'input');
       }
       },t3); 
-      
     //phone
     setTimeout(function(){
       const input111= document.getElementById('input-111');
@@ -50,7 +49,6 @@ var timer = setInterval(function() {
           triggerEvent(input111, 'input');
       }
       },t4); 
-      
     //cmnd
     setTimeout(function(){
       const input114= document.getElementById('input-114');
@@ -59,7 +57,6 @@ var timer = setInterval(function() {
           triggerEvent(input114, 'input');
       }
       },t5);  
-
     //noi cap
     setTimeout(function(){
       const input123= document.getElementById('input-123');
@@ -68,8 +65,6 @@ var timer = setInterval(function() {
           triggerEvent(input123, 'input');
       }
       },t6);       
-
-
     //ngay cap
     setTimeout(function(){
       var input119 = document.getElementById('input-119');
@@ -86,29 +81,23 @@ var timer = setInterval(function() {
             triggerEvent(checkbox, 'change');
         }
       },tcheckbox);
-    
-    // Stop the interval
     clearInterval(timer);
   }
 }, 100);
----------------------------phan 2
+//---------------------------phan 2
 let inputElement;
 let timer = setInterval(function() {
   inputElement = document.getElementById('input-25');
   if (inputElement) {
     inputElement.click();
-    
     document.querySelector('.v-list-item__content').click();
-    
     let button;
 let timer1 = setInterval(function(){
   button = document.querySelector('button[type="button"][step="3"].btn-main.next-step');
   if(button){
     button.click();
-    
     clearInterval(timer1);
   }
-  
 },100);
     clearInterval(timer);
   }
